3a6b650a0934d84f38e02195a6e54b264def8c61
royyan005 <royyan005@gmail.com>
2022-05-25 23:15:46 +0700

    fix host

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('rifsa','root','',{
-    host: "localhost",
+const db = new Sequelize('rifsa-dummy','rifsa-dummy','rifsa123',{
+    host: "34.101.143.232",
     dialect: "mysql"
 })
 
