4b5364080e7d514c7cfab46ecd42c77753efad6a
royyan005 <royyan005@gmail.com>
2022-05-25 23:51:58 +0700

    fix username for database host

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('rifsa-dummy','rifsa-dummy','rifsa123',{
+const db = new Sequelize('rifsa-dummy','root','rifsa123',{
     host: "34.101.143.232",
     dialect: "mysql"
 })
