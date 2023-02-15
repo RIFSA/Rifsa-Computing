595310452036f00af0c8c9892e21fe74388c1db1
royyan fajrul falah <royyan005@gmail.com>
2023-02-15 20:28:52 +0700

    change database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('rifsa-dummy','root','rifsa123',{
-    host: "34.101.143.232",
+const db = new Sequelize('brjt28aiaay5uf5x0to7','utdid4lmcvjgulrr','wqSiCi7aWyd6LKZNkMfJ',{
+    host: "brjt28aiaay5uf5x0to7-mysql.services.clever-cloud.com",
     dialect: "mysql"
 })
 
