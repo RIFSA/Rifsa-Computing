90bd7f782c57328a41be842ef8bb72ac983c5668
royyan fajrul falah <royyan005@gmail.com>
2023-06-27 00:11:49 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('rifsanodejs','user1','123123123',{
-    host: "35.222.118.43",
+    host: "34.69.207.6",
     dialect: "mysql"
 })
 
